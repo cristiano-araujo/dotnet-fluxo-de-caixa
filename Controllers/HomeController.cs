79a97ebb1e45af5a531cf7115166eb652ba8d0fc
Cristiano Araújo <cristiano112715@gmail.com>
2023-09-30 20:34:32 -0300

    versionando os meus dados

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using dotnet_fluxo_de_caixa.Models;
+using dotnet_fluxo_de_caixa.db;
+
+namespace dotnet_fluxo_de_caixa.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger, MeuDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    private MeuDbContext _context;
+
+    public IActionResult Index()
+    {
+        var lista = _context.Caixas.ToList();
+        ViewBag.lista = lista;
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    // pagina test criada
+    public IActionResult Test()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
